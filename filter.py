68a2e904357c8f081675dc822a9c8eb8c037832f
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-12-08 15:10:08 +0100

    Test for pipeline

--- filter.py
+++ filter.py
@@ -4,15 +4,18 @@ import numpy as np
 
 def Dilate(image):
     kernel = np.ones((5, 5), np.uint8)
-    img_dilation = cv2.dilate(image, kernel, iterations=1)
-    cv2.imwrite('filtered_img/fond_discord.jpeg', img_dilation)
+    cv2.dilate(image, kernel, iterations=1)
+    return image
 
 
 def Blur(image):
-    dst = cv2.GaussianBlur(image, (59, 59), cv2.BORDER_DEFAULT)
-    cv2.imwrite('filtered_img/lena.jpeg', dst)
+    cv2.GaussianBlur(image, (59, 59), cv2.BORDER_DEFAULT)
+    return image
 
 
 def GrayScale(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('filtered_img/lena.jpg', gray)
+    cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import cv2
+import os
 import numpy as np
+from filter import Dilate, Blur, GrayScale
 
-from filter import Dilate
+liste = os.listdir('img')
+
 
 Dilate(cv2.imread('img/fond_discord.jpeg'))
+cv2.imwrite('filtered_img/fond_discord.jpeg')
+
+
+for i in liste:
+    GrayScale(cv2.imread(i))
+
+
+    print('filtered_' + i)
+
 
