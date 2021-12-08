6a1f1117d9c27017877e1e83904a85d18363a114
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-12-08 14:24:25 +0100

    Add exceptions controllers

--- filter.py
+++ filter.py
@@ -3,19 +3,25 @@ import numpy as np
 
 
 def Dilate(image):
-    kernel = np.ones((5, 5), np.uint8)
-    cv2.dilate(image, kernel, iterations=1)
-    return image
+    try:
+        kernel = np.ones((5, 5), np.uint8)
+        img_dilation = cv2.dilate(image, kernel, iterations=1)
+        return img_dilation
+    except cv2.error:
+        print("Image not found")
 
 
 def Blur(image):
-    cv2.GaussianBlur(image, (59, 59), cv2.BORDER_DEFAULT)
-    return image
+    try:
+        dst = cv2.GaussianBlur(image, (59, 59), cv2.BORDER_DEFAULT)
+        return dst
+    except cv2.error:
+        print("Image not found")
 
 
 def GrayScale(image):
-    cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return image
-
-
-
+    try:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return gray
+    except cv2.error:
+        print("Image not found")
