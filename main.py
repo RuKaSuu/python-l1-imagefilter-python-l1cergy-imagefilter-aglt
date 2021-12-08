1f56fa75269d34c24667ddc26f301833a2f18e90
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-12-08 12:05:44 +0100

    Add filter function

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import cv2
 import numpy as np
 
+#Dilate
 img = cv2.imread('img/fond_discord.jpeg')
-
 kernel = np.ones((5, 5), np.uint8)
+
+#GrayScale
 image = cv2.imread('img/lena.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+#Blur
 src = cv2.imread('img/lena.jpeg', cv2.IMREAD_UNCHANGED)
 dst = cv2.GaussianBlur(src, (59, 59), cv2.BORDER_DEFAULT)
-
 
 img_dilation = cv2.dilate(img, kernel, iterations=1)
 
@@ -19,7 +21,4 @@ cv2.imwrite('filtered_img/lena.jpg', gray)
 cv2.imwrite('filtered_img/lena.jpeg', dst)
 
 cv2.waitKey(0)
-
-
-
 
