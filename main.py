77cf250665a135a5573792b240015ab14c5f8e33
Theo Landemaine <theo.landemaine@hotmail.fr>
2021-12-09 11:01:58 +0100

    Add error system

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import cv2
 import os
 import sys
+from filter import Dilate, Blur, GrayScale
+
 
 args = sys.argv
 
-from filter import Dilate, Blur, GrayScale
 
 liste = os.listdir('img')
-
-# Dilate(cv2.imread('img/fondDiscord.jpeg'))
-# cv2.imwrite('filtered_img/fondDiscord.jpeg')
-
-# for i in range(0 , len(args)):
-#     arg = args[i]
-#     print(arg)
-#     if arg == '--filters':
 
 
 for img_name in liste:
 
-    if not img_name.endswith('.jpeg' or '.png' or '.jpg'):
-        print("its  ot a jpg or png or jpeg, it's a " + img_name)
+    if not img_name.endswith(('.jpg', '.png', '.jpeg')):
+        print("its not a jpg or png or jpeg, it's a " + img_name)
     else:
         img_path = f'img/{img_name}'
         image = cv2.imread(img_path)
@@ -32,4 +25,3 @@ for img_name in liste:
         cv2.imwrite(output, image)
 
         print("Conversion ok " + img_name)
-        # print('filtered_img/' + i)
