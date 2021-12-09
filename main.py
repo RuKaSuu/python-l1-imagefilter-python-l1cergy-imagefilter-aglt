c20400a55ecdea8c116fc3cf7a10a299320ad5f7
Lucas Goncalves <lucasgoncalves@free.fr>
2021-12-09 11:57:04 +0100

    Add filter variable

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ for img_name in liste:
         img_path = f'img/{img_name}'
         image = cv2.imread(img_path)
         image = GrayScale(image)
-        image = Blur(image)
-        image = Dilate(image)
-
+        image = Blur(image, (59,59))
+        image = Dilate(image, 50)
         output = f'filtered_img/{img_name}'
         cv2.imwrite(output, image)
 
